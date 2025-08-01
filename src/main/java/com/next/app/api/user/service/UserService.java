2e256271ab0d897c62aba0fecc3629f78866de94
pgh0407 <qpwee123@gmail.com>
2025-08-01 14:06:38 +0900

    상품 관리 api 추가

--- src/main/java/com/next/app/api/user/service/UserService.java
+++ src/main/java/com/next/app/api/user/service/UserService.java
@@ -2,12 +2,14 @@ package com.next.app.api.user.service;
 
 import com.next.app.api.user.entity.User;
 import com.next.app.api.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class UserService {
     
@@ -29,16 +31,18 @@ public class UserService {
     public User createUser(User user) {
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new RuntimeException("이미 존재하는 이메일입니다: " + user.getEmail());
+
         }
+        log.info("Creating user: " + user.getEmail());
         return userRepository.save(user);
+
     }
     
     public User updateUser(Long id, User userDetails) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다: " + id));
-        
-        user.setName(userDetails.getName());
-        user.setPhone(userDetails.getPhone());
+
+        System.out.println("Creating user: " + user.getEmail());
         
         return userRepository.save(user);
     }
