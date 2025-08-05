bbd5fb351282eaaed87e9b961360b11ad3cda228
pgh0407 <qpwee123@gmail.com>
2025-08-05 10:41:07 +0900

    (chore) HelloController file edit2

--- src/main/java/com/next/app/api/user/controller/HelloController.java
+++ src/main/java/com/next/app/api/user/controller/HelloController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    @Operation(summary = "Hello 메시지 반환", description = "간단한 인사말을 반환합니다.")
+    @Operation(summary = "Hello 메시지 반환", description = "잔인한 QUEEN이며 SCENE이자 JONGGYEOL. 이토록 거대한 내 안의 EXPLOSION 내 모든 세포 별로부터 만들어져.")
     public String hello() {
         return "Hello, Spring Boot with Swagger!";
     }
